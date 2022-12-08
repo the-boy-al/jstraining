91623f8c2ceef109672f2241f0fd24ef2df7c522
Alyamdar <alyamdar.askerov@nu.edu.kz>
2022-12-09 02:00:00 +0600

    correct Task 10

--- task10.js
+++ task10.js
@@ -1,9 +1,22 @@
-function multiply2Max(num){
-    console.log(num);
+
+function multiply2Max (num)  {
+    function multiply2MaxInt (num2) {
+        function multiply2MaxInt2 (num3){
+            let arr = [num, num2, num3];
+            arr.sort((a,b) => {
+                if(a > b) return 1;
+                if(a < b) return -1;
+                return 0;
+            });
+            return arr[0] * arr[1];
+        }
+        return multiply2MaxInt2;
+    }
+    return multiply2MaxInt;
 }
 
 
-//console.log(multiply2Max(1)(2)(3)); // 2
-//console.log(multiply2Max(4)(4)(1)); // 4
-//console.log(multiply2Max(3)(0)(5)); // 0
-//multiply2Max(3)(0)(5)
+
+console.log(multiply2Max(1)(2)(3)); // 2
+console.log(multiply2Max(4)(4)(1)); // 4
+console.log(multiply2Max(3)(0)(5)); // 0
